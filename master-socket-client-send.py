26f35a4f8c8b41a08cd2de21ee8cf61fc4099049
Robbie Jiao <xj.jiao@gmail.com>
2016-08-13 19:49:44 -0700

    master-socket-client-send.py: fixed some errors

--- master-socket-client-send.py
+++ master-socket-client-send.py
@@ -6,6 +6,8 @@
 import socket
 import sys
 
+endMark = '$'
+
 if len(sys.argv) >3:
     print >>sys.stderr, "Usage: master-socket-cilent-send <message>\n master-socket-client-send -f <file path>"
     exit(-1)
@@ -13,14 +15,15 @@ elif len(sys.argv) == 3:
     script, parameter, messageFile = sys.argv
     if parameter == '-f':
         with open(messageFile) as msgfile:
-            messageTosend = msgfile.read().replace('\n', '')
+            messageTosend = msgfile.read().replace('\n', '') + endMark
     else:
         print >>sys.stderr, "Usage: master-socket-cilent-send <message>\n master-socket-client-send -f <file path>"
         exit(-1)
 elif len(sys.argv) == 2:
     script, messageTosend = sys.argv
+    messageTosend += endMark
 else:
-    messageTosend = 'This is the message. It will be repeated.'
+    messageTosend = 'This is the message. It will be repeated.' + endMark
 #Create a TCP/IP client
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -34,16 +37,18 @@ try:
     #Send data
     print >>sys.stderr, 'sending "%s"' % messageTosend
     clientsocket.sendall(messageTosend)
-    
+    print messageTosend
     #Look for the response
     amount_received = 0
     amount_expected = len(messageTosend)
-    
+
     while amount_received < amount_expected:
-        data = clientsocket.recv(16)
+        data = clientsocket.recv(128)
         amount_received += len(data)
         print >>sys.stderr, 'received "%s"' % data
-        
+        if data.endswith(endMark):
+            break
+
 finally:
     print >>sys.stderr, 'closing client'
     clientsocket.close()
