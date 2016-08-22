16c4dc4315194aa7f04c2ce821c79db3bf9a4159
Robbie Jiao <xj.jiao@gmail.com>
2016-08-22 00:31:37 -0700

    Added "shell=True" so that ~ can be recoganized by subprocess call

--- subfunctions/akquiltsync.py
+++ subfunctions/akquiltsync.py
@@ -20,8 +20,8 @@ sha1 = '4200ebb7bb687dbfa1391ae6d615542fa8c741db'
 
 
 #need to sync
-tmp = 'mkdir'
-subprocess.check_call([tmp, '~/tmp'])
+cmd = gminHome + 'bin/akquiltsync' + ' -k' + ' kernelType' + ' sha1'
+subprocess.check_call(cmd, shell=True)
 #subprocess.check_call([gminHome + 'bin/akquiltsync', '-k', kernelType, sha1])
 
 #clone the remote repo
@@ -29,12 +29,17 @@ subprocess.call(['git', 'clone', remoteUrl])
 
 #After akquiltsync finishes, the repo is on a specific branch created by the script.
 #Copy the patches to github repo.
-subprocess.call(['rm', '-rf', localRepo + 'uefi/cht-m1stable/patches'])
-subprocess.call(['cd', gminHome + 'uefi/cht-m1stable'])
+cmd = 'rm' + ' -rf ' + localRepo + 'uefi/cht-m1stable/patches'
+subprocess.call(cmd, shell=True)
+
+cmd = 'cd ' + gminHome + 'uefi/cht-m1stable'
+subprocess.call(cmd, shell=True)
+
 res = subprocess.Popen(['find', 'patches'], stdout=subprocess.PIPE)
-subprocess.check_call(['cpio', '-pdmuv', localRepo + 'uefi/cht-m1stable'], stdin=res.stdout)
+cmd = 'cpio ' + '-pdmuv ' + localRepo + 'uefi/cht-m1stable'
+subprocess.check_call(cmd, stdin=res.stdout, shell=True)
 
 #Update technical debt report
-subprocess.call(['cd', gminHome])
-subprocess.call(['./bin/akgroup', '-c', '-d', 'uefi/cht-m1stable/patches', '>', localrepo + 'uefi/cht-m1stable/TechnicalDebtSummary.csv'])
-subprocess.call(['./bin/akgroup', '-cv', '-d', 'uefi/cht-m1stable/patches', '>', localrepo + 'uefi/cht-m1stable/TechnicalDebt.csv'])
+subprocess.call('cd ' + gminHome, shell=True)
+subprocess.call('./bin/akgroup ' + '-c ', '-d ' + 'uefi/cht-m1stable/patches ', '> ' + localrepo + 'uefi/cht-m1stable/TechnicalDebtSummary.csv', shell=True)
+subprocess.call('./bin/akgroup ' + '-cv ' + '-di ' + 'uefi/cht-m1stable/patchesi ' + '> ' + localrepo + 'uefi/cht-m1stable/TechnicalDebt.csv', shell=True)
